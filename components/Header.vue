f10831d9c5882b6824decf59f1780d4c50feabca
Votre Nom <befrigeo@gmail.com>
2025-05-19 16:50:46 +0200

    Initial commit 5

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="flex flex-col items-center justify-center py-8">
       <img
-        src="https://st4.depositphotos.com/18826394/40972/v/450/depositphotos_409726402-stock-illustration-hot-pizza-pizzeria-logo-icon.jpg"
+        src="https://dcassetcdn.com/design_img/611204/113998/113998_3972827_611204_image.png"
         alt="Logo Bella Napoli"
         class="w-32 h-32 object-contain rounded-full shadow-lg border-4 border-white"
       />
